becf5b31a821ba25ee6a4fcffe9a2be92024f194
JJ <srineshram@gmail.com>
2022-06-12 21:30:31 -0400

    preliminary methods completed

--- Part B/js/BinarySearchTree.js
+++ Part B/js/BinarySearchTree.js
@@ -19,34 +19,149 @@ export default class BinarySearchTree {
     generateKey() {
         let key = "";
         for (let i = 0; i < this.keyLength; i++) {
-            let randomNum = Math.floor(Math.random() * 36);
+            let randomNum = Math.floor(Math.random() * 26);
             let randomChar;
-            if (randomNum < 10) {
-                randomNum += 48;
-                randomChar = String.fromCharCode(randomNum);
-            }
-            else {
-                randomNum += 55;
-                randomChar = String.fromCharCode(randomNum);
-            }
+            randomNum + 97;
+            randomChar = String.fromCharCode(randomNum);
             key += randomChar;
         }
         return key;
     }
 
+    putValueRecursively(key, value, testNode) {
+        if(key < testNode.key){
+            if(testNode.left == null){
+                testNode.left = new Node(key, value, testNode, null, null);
+                this.size++;
+                return;
+            }
+            else{
+                this.putValueRecursively(key, value, testNode.left);
+            }
+        }
+        else if(key == testNode.key){
+            testNode.data = value;
+            return;
+        }
+        else{
+            if(testNode.right == null){
+                testNode.right = new Node(key, value, testNode, null, null);
+                this.size++;
+                return;
+            }
+            else{
+                this.putValueRecursively(key, value, testNode.right);
+            }
+        }
+    }
+
     // @todo - YOU MUST DEFINE THIS METHOD
     putValue(key, value) {
-
+        
     }
 
+
+    getValueRecursively(key, testNode){
+        if(testNode.key < key){
+            if (testNode.left == null) {
+                return null;
+            }
+            else {
+                return this.getValueRecursively(key, testNode.left);
+            }
+        }
+        else if(testNode.key == key){
+            return testNode.data;
+        }
+        else{
+            if (testNode.right == null) {
+                return null;
+            }
+            else {
+                return this.getValueRecursively(key, testNode.right);
+            }
+        }
+    }
     // @todo - YOU MUST DEFINE THIS METHOD
     getValue(key) {
-        return null;
+        if(this.root == null){
+            return null;
+        }
+        else{
+            return this.getValueRecursively(key, this.root);
+        }
+        
     }
+
 
     // @todo - YOU MUST DEFINE THIS METHOD
     removeValue(key) {
+        traveller = this.root;
+        found = false;
+        while(!found){
+            if(traveller.key == key){
+                if(traveller.left != null){
+                    largest = traveller.left;
+                    while(largest.right != null){
+                        largest = largest.right;
+                    }
+                    traveller.key = largest.key;
+                    traveller.data = largest.data;
 
+                    if(largest == largest.parent.left){
+                        largest.parent.left = largest.left;
+                    }
+                    else{
+                        largest.parent.right = largest.left;
+                    }
+                }
+                else if(traveller.right != null){
+                    smallest = traveller.right;
+                    while(smallest.left != null){
+                        smallest = smallest.left;
+                    }
+                    traveller.key = smallest.key;
+                    traveller.data = smallest.data;
+
+                    if(smallest == smallest.parent.right){
+                        smallest.parent.right = smallest.right;
+                    }
+                    else{
+                        smallest.parent.left = smallest.right;
+                    }
+                }
+                else{
+                    if (traveller == this.root){
+                        this.root = null;
+                    }
+                    else if(traveller == traveller.parent.left){
+                        traveller.parent.left = null;
+                    }
+                    else{
+                        traveller.parent.right = null;
+                    }
+                }
+                this.size--;
+                found = true;
+            }
+            else if(this.key < traveller.key)
+            {
+                if(traveller.left == null){
+                    return;
+                }
+                else{
+                    traveller = traveller.left;
+                }
+            }
+            else{
+                if(traveller.right == null){
+                    return;
+                }
+                else{
+                    traveller = traveller.right;
+                }
+            }
+        }
     }
 
     toStringRecursively(traveller, level) {
